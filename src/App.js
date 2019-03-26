06dd42d20b8cba91348616fd60c606e9223c3002
erkbamyaci <erkbamyaci@gmail.com>
2019-03-26 09:05:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Router, Switch, Route} from "react-router-dom";
+import Home from "./routes/home/Home";
+import history from "./history";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+
+    render() {
+
+        return (
+
+            <Router history={history}>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/routes/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.scss";
+
+export default class Home extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+                HOME
+            </div>
+        )
+    }
+}
